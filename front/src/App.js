11c09cfaf8ac48d475bd44f48eb07fd0468c5cbb
임건혁 <gunhyuck11@naver.com>
2020-05-22 02:36:33 +0900

    [Add] Temporary login page, S02P31A104-6

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
       </BrowserRouter>
     </div>
   );
