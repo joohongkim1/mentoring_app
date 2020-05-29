d66306a2a789039c335538e05870192be4348130
임건혁 <gunhyuck11@naver.com>
2020-05-29 12:56:48 +0900

    [Add] Save token at Cookie, S02P31A104-6

--- front/src/index.js
+++ front/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import rootReducer from './modules';
-
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 
 const store = createStore(
   rootReducer,
