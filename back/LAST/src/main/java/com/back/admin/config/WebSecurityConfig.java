05694928044d19477e00e4b709b284c9d4c9288f
김주홍 <wnghd9938@naver.com>
2020-06-08 14:02:27 +0900

    temp

--- back/LAST/src/main/java/com/back/admin/config/WebSecurityConfig.java
+++ back/LAST/src/main/java/com/back/admin/config/WebSecurityConfig.java
@@ -2,22 +2,13 @@ package com.back.admin.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/resources/**").antMatchers("/api/**")
-                .antMatchers("/**");
-    }
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -53,4 +44,3 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .roles("GUEST");
     }
 }
-
